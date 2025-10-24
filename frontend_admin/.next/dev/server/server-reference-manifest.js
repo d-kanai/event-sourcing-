cee4939903949a535dbb4eab027f2997a62bf6d8
d.kanai <d.kanai@dkanais-MacBook-Pro.local>
2025-10-24 12:11:23 +0900

    feat: add admin frontend and event schemas

--- frontend_admin/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"XkKfkfypj2Cm+C4Yq54zX4xhIlEneapCk+ZEq7gODHA=\"\n}"
--- frontend_admin/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"lPZHlCra6fDudj4tArR3QQbY09pKPslDoks5Sb2R/vQ=\"\n}"
